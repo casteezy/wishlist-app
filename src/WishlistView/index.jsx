fb89bbd44444dd09520030ce72eeab67f2e044cc
Trina Castillo <trina.castillo@expeditors.com>
2016-12-19 12:22:47 -0800

    Ejected project to add scss webpack support

--- src/WishlistView/index.jsx
+++ src/WishlistView/index.jsx
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import logo from '../App/logo.svg';
+import './WishlistView.scss';
 
 class WishlistView extends Component {
     render() {
         return (
-            <div className="App">
+            <div className="App WishlistView">
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
                     <h2>Welcome to React</h2>
