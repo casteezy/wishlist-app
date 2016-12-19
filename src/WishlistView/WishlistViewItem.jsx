6d9a7ddf7f990217168fe6f91376ec100896ed4e
Trina Castillo <trina.castillo@expeditors.com>
2016-12-19 13:24:37 -0800

    Render items using props

--- src/WishlistView/WishlistViewItem.jsx
+++ src/WishlistView/WishlistViewItem.jsx
@@ -1,31 +1,29 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Media} from 'react-bootstrap';
 
-class WishlistItem extends Component {
-    render() {
-        return (
-            <div className="WishlistViewItem">
-                <Media>
-                    <Media.Left>
-                        <img width="150" height="150" src="http://placehold.it/150x150" alt="placeholder"/>
-                    </Media.Left>
+function WishlistViewItem({imgSrc, title, detail, description}) {
+    return (
+        <div className="WishlistViewItem">
+            <Media>
+                <Media.Left>
+                    <img class="WishlistViewItem--Image" src={imgSrc} alt="placeholder"/>
+                </Media.Left>
+                <Media.Body>
+                    <Media.Heading className="WishlistViewItem--Title"><a href="#">{title}</a></Media.Heading>
                     <Media.Body>
-                        <Media.Heading className="WishlistViewItem--Title"><a href="#">Lorem Ipsum Dolores</a></Media.Heading>
-                        <Media.Body>
-                            <p className="WishlistViewItem--Details"><strong>$10.00 at Amazon, Qty: 8</strong></p>
-                            <p className="WishlistViewItem--Description">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-                                sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra
-                                turpis.</p>
-                            <ul className="list-inline WishlistViewItem--LinkBar">
-                                <li className="WishlistViewItem--LinkBarItem"><a href="#"><span className="glyphicon glyphicon-ok"></span> Mark as Purchased</a></li>
-                                <li className="WishlistViewItem--LinkBarItem"><a href="#"><span className="glyphicon glyphicon-question-sign"></span> Ask About This Item</a></li>
-                            </ul>
-                        </Media.Body>
+                        <p className="WishlistViewItem--Details"><strong>{detail}</strong></p>
+                        <p className="WishlistViewItem--Description">{description}</p>
+                        <ul className="list-inline WishlistViewItem--LinkBar">
+                            <li className="WishlistViewItem--LinkBarItem"><a href="#"><span
+                                className="glyphicon glyphicon-ok"></span> Mark as Purchased</a></li>
+                            <li className="WishlistViewItem--LinkBarItem"><a href="#"><span
+                                className="glyphicon glyphicon-question-sign"></span> Ask About This Item</a></li>
+                        </ul>
                     </Media.Body>
-                </Media>
-            </div>
-        );
-    }
+                </Media.Body>
+            </Media>
+        </div>
+    );
 }
 
-export default WishlistItem;
+export default WishlistViewItem;
--- src/WishlistView/index.jsx
+++ src/WishlistView/index.jsx
@@ -1,26 +1,48 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Grid, Row, Col, PageHeader} from 'react-bootstrap';
 import WishlistViewItem from './WishlistViewItem';
 
 import './WishlistView.scss';
 
-class WishlistView extends Component {
-    render() {
+function WishlistView() {
+    const ownerName = 'Jane';
+    const items = [{
+        title: 'Lorem Ipsum Dolores',
+        detail: '$21.99 at Amazon, Qty: 1',
+        description: 'Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis.',
+    }, {
+        title: 'Forcem Ipsum Rebel Base',
+        detail: '$3.95 at Target, Qty: 8',
+        description: `Stretch out with your feelings. You see, you can do it. I call it luck. In my experience, there's no such thing as luck. Look, going good against remotes is one thing. Going good against the living? That's something else. Looks like we're coming up on Alderaan. No, my father didn't fight in the wars. He was a navigator on a spice freighter. That's what your uncle told you. He didn't hold with your father's ideals. Thought he should have stayed here and not gotten involved. You fought in the Clone Wars? Yes, I was once a Jedi Knight the same as your father.`,
+    }];
+
+    const wishlistViewItems = items.map(({title, detail, description}) => {
         return (
-            <div className="WishlistView container">
-
-                <Grid>
-                    <Row className="show-grid">
-                        <Col md={10} mdOffset={1}>
-                            <PageHeader className="page-title">Jane's Wishlist</PageHeader>
-
-                            <WishlistViewItem />
-                        </Col>
-                    </Row>
-                </Grid>
-            </div>
+            <Row>
+                <Col md={10} mdOffset={1}>
+                    <WishlistViewItem
+                        imgSrc='http://placehold.it/200x200'
+                        title={title}
+                        detail={detail}
+                        description={description}
+                    />
+                </Col>
+            </Row>
         );
-    }
+    });
+
+    return (
+        <div className="WishlistView container">
+            <Grid>
+                <Row>
+                    <Col md={10} mdOffset={1}>
+                        <PageHeader className="page-title">{ownerName}'s Wishlist</PageHeader>
+                    </Col>
+                </Row>
+                {wishlistViewItems}
+            </Grid>
+        </div>
+    );
 }
 
 export default WishlistView;
