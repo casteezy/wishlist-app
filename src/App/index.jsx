12dba1395c84738c3197fee0ef0da3395fb77368
Trina Castillo <trina.castillo@expeditors.com>
2016-12-19 11:46:31 -0800

    Rearranging folder structure to match styleguide

---
+++ src/App/index.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import WishlistView from '../WishlistView';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <WishlistView />
+    );
+  }
+}
+
+export default App;
