78be427aa6e3f314b9499a10344cf2a25f747822
casteezy <tricx3illo@gmail.com>
2016-12-23 14:00:45 -0800

    fix imports to match file name

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './App/store';
-import App from './App';
+import App from './App/App';
 import './styles';
 
 
