6fbecfde8c9679e418ce8da0c345753de637c78c
Trina Castillo <trina.castillo@expeditors.com>
2017-04-17 17:45:17 -0700

    Added react router + new REST mappings

--- client/src/main/Main.js
+++ client/src/main/Main.js
@@ -3,13 +3,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
 import store from './store';
 import App from './components/App';
 
-
+// TODO: list/:id/
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router history={browserHistory}>
+      <Route path="/" component={App} />
+    </Router>
   </Provider>,
   document.getElementById('root'),
 );
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+// http://www.vinaysahni.com/best-practices-for-a-pragmatic-restful-api
 const express = require('express');
 
 const app = express();
@@ -8,7 +9,53 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`);
+const userRoute = '/api/v1/user/:userId';
+const wishlistRoute = `${userRoute}/lists`;
+
+// Wishlists
+app.get(`${wishlistRoute}`, (req, res) => {
+  res.send('Getting data on all lists');
+});
+
+app.route(`${wishlistRoute}/:listId`)
+  .get((req, res) => {
+    res.send('Getting specific list');
+  })
+  .put((req, res) => {
+    res.send('Updating specific list');
+  })
+  .delete((req, res) => {
+    res.send('Delete a specific list');
+  });
+
+app.route(`${wishlistRoute}/:listId/items`)
+  .get((req, res) => {
+    res.send('Getting all items in a list');
+  });
+
+// Wishlist Items
+app.route(`${wishlistRoute}/:listId/items`)
+  .post((req, res) => {
+    res.send('Creating a new item');
+  });
+
+app.route(`${wishlistRoute}/:listId/items/:itemId`)
+  .get((req, res) => {
+    res.send('Get a specific item');
+  })
+  .put((req, res) => {
+    res.send('Update a specific item');
+  })
+  .delete((req, res) => {
+    res.send('Delete a specific item');
+  });
+
+
+app.listen(port, (err) => {
+  // TODO `use` middleware error handling
+  if (err) {
+    return console.error('something bad happened', err);
+  }
+  return console.log(`Example app listening on port ${port}!`);
 });
 
